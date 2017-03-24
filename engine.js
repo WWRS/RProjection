4a0b280d512e1733547db5f7b18984210c47ea6a
RShields <playnow254@gmail.com>
2017-03-24 09:39:40 -0700

    Finished framework
    Completed the stylesheet.
    Finished up the framework on engine.js and projection.html.
    
    TODO: Actually code the renderer.

---
+++ engine.js
@@ -0,0 +1,16 @@
+var i,j,k,x,y,z,c;
+function update() {
+	i = $("#i").val();
+	j = $("#j").val();
+	k = $("#k").val();
+	x = $("#x").val();
+	y = $("#y").val();
+	z = $("#z").val();
+	c = $("#c").val();
+	
+	
+}
+
+$("#run").click( update );
+
+$( document ).ready( update );
