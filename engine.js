c89e8e4c7c7112697bf4e17e29155623cabdd645
RShields <playnow254@gmail.com>
2017-03-24 13:18:19 -0700

    Actually finished framework
    I lied on the other one...
    
    Big:
    Added math for input and drawing lines.
    
    Small:
    Renamed run -> gobutton
    Fixed viewport scaling
    
    TODO: Actually code the thing.

--- engine.js
+++ engine.js
@@ -1,16 +1,57 @@
-var i,j,k,x,y,z,c;
-function update() {
-	i = $("#i").val();
-	j = $("#j").val();
-	k = $("#k").val();
-	x = $("#x").val();
-	y = $("#y").val();
-	z = $("#z").val();
-	c = $("#c").val();
-	
-	
+var ctx = $("#viewport")[0].getContext("2d");
+
+function drawPolar(angle, length) {
+	ctx.beginPath();
+	ctx.moveTo(0,0);
+	ctx.lineTo(300*length*Math.cos(angle), -300*length*Math.sin(angle));
+	ctx.stroke();
 }
 
-$("#run").click( update );
+function drawToEdge(angle) {
+	var sina = Math.sin(angle),
+		cosa = Math.cos(angle),
+		tana = Math.tan(angle);
+	
+	ctx.beginPath();
+	ctx.moveTo(0,0);
+	if (Math.abs(sina) > Math.abs(cosa)) {
+		ctx.lineTo(300/tana,-300*Math.sign(sina));
+	} else {
+		ctx.lineTo(300*Math.sign(cosa),-300*tana);
+	}
+	ctx.stroke();
+}
+
+function update() {
+	var i = $("#i").val(),
+		j = $("#j").val(),
+		k = $("#k").val(),
+		x = $("#x").val(),
+		y = $("#y").val(),
+		z = $("#z").val(),
+		c = $("#c").val();
+	var lijk = Math.sqrt(i*i + j*j + k*k),
+		lxyz = Math.sqrt(x*x + y*y + z*z);
+	i /= lijk;
+	j /= lijk;
+	k /= lijk;
+	x /= lxyz;
+	y /= lxyz;
+	z /= lxyz;
+	
+	ctx.fillStyle = "#eeeeee";
+	ctx.fillRect(0,0,600,600);
+	
+	ctx.translate(300,300);
+	ctx.lineWidth = 1;
+	
+	ctx.strokeStyle = "#ee1111";
+	drawToEdge(Math.PI/2);
+	ctx.strokeStyle = "#11ee11";
+	drawPolar(1, 0.5);
+	ctx.strokeStyle = "#1111ee";
+}
+
+$("#gobutton").click( update );
 
 $( document ).ready( update );
