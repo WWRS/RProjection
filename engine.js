4f4eb06b4e3a993996a046b4842f455d7d3e1e0e
RShields <playnow254@gmail.com>
2017-03-25 07:52:43 -0700

    Draw axes
    Big:
    Added total support for drawing axes. (I may still revist this later)
    
    Small:
    ctx translates.
    changed drawToEdge to drawFunc (for functional notation) and changed
    related code.
    
    TODO: Draw the vector

--- engine.js
+++ engine.js
@@ -1,5 +1,16 @@
 var ctx = $("#viewport")[0].getContext("2d");
 
+function getVal(id) {
+	var num = $(id).val().replace(/[^\d-\.]/g, "");
+	if (num == "") {
+		return 1;
+	} else if (num == '-'){
+		return 1;
+	} else {
+		return num;
+	}
+}
+
 function drawPolar(angle, length) {
 	ctx.beginPath();
 	ctx.moveTo(0,0);
@@ -7,31 +18,23 @@ function drawPolar(angle, length) {
 	ctx.stroke();
 }
 
-function drawToEdge(angle) {
-	var sina = Math.sin(angle),
-		cosa = Math.cos(angle),
-		tana = Math.tan(angle);
-	
+function drawFunc(x, y) {
 	ctx.beginPath();
 	ctx.moveTo(0,0);
-	if (Math.abs(sina) > Math.abs(cosa)) {
-		ctx.lineTo(300/tana,-300*Math.sign(sina));
-	} else {
-		ctx.lineTo(300*Math.sign(cosa),-300*tana);
-	}
+	ctx.lineTo(300*x, -300*y);
 	ctx.stroke();
 }
 
 function update() {
-	var i = $("#i").val(),
-		j = $("#j").val(),
-		k = $("#k").val(),
-		x = $("#x").val(),
-		y = $("#y").val(),
-		z = $("#z").val(),
-		c = $("#c").val();
+	var i = getVal("#i"),
+		j = getVal("#j"),
+		k = getVal("#k"),
+		x = getVal("#x"),
+		y = getVal("#y"),
+		z = getVal("#z"),
+		c = getVal("#c");
 	var lijk = Math.sqrt(i*i + j*j + k*k),
-		lxyz = Math.sqrt(x*x + y*y + z*z);
+		lxyz = Math.sqrt(x*x + y*y + z*z)*Math.sign(c);
 	i /= lijk;
 	j /= lijk;
 	k /= lijk;
@@ -40,18 +43,32 @@ function update() {
 	z /= lxyz;
 	
 	ctx.fillStyle = "#eeeeee";
-	ctx.fillRect(0,0,600,600);
+	ctx.fillRect(-300,-300, 600,600);
 	
-	ctx.translate(300,300);
-	ctx.lineWidth = 1;
+	if ( x==0 && y==0 ){
+		ctx.strokeStyle = "#ee1111";
+		drawFunc(1,0);
+		ctx.strokeStyle = "#11ee11";
+		drawFunc(0,1);
+	} else {
+		ctx.strokeStyle = "#ee1111";
+		var x2=x*x,
+			y2=y*y,
+			z2=z*z;
+		drawPolar( -Math.acos( -y/( Math.sqrt(1-x*x)*Math.sqrt(1-z*z) ) ) , 1-Math.abs(x));
+		ctx.strokeStyle = "#11ee11";
+		drawPolar( -Math.acos( x/( Math.sqrt(1-y*y)*Math.sqrt(1-z*z) ) ) , 1-Math.abs(y));
+		ctx.strokeStyle = "#1111ee";
+		drawPolar(Math.PI/2, 1-Math.abs(z));
+	}
 	
-	ctx.strokeStyle = "#ee1111";
-	drawToEdge(Math.PI/2);
-	ctx.strokeStyle = "#11ee11";
-	drawPolar(1, 0.5);
-	ctx.strokeStyle = "#1111ee";
 }
 
 $("#gobutton").click( update );
 
-$( document ).ready( update );
+$( document ).ready( function() {
+	ctx.translate(300,300);
+	ctx.lineWidth = 1;
+	
+	update();
+});
